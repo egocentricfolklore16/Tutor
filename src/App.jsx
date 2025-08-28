b6227164ace639a0ff1f6ee7fbb9caeca1e7d3ac
Egocentricfolklore16 <bakarecaleb02@gmail.com>
2025-08-28 15:06:33 +0100

    configured the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { Children, StrictMode } from "react"
+import Sidebar from "./components/Layout/Sidebar"
+import { createBrowserRouter,RouterProvider } from "react-router"
 
-function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+  const router = createBrowserRouter([
+        {
+          element:(
+            <TaskProvider>
+            <AppLayout />
+          </TaskProvider>
+          ),
+          
+        children: [
+        {
+          path: "/",
+          element: <App/>,
+        },
+      ]
+    }
+  ]
+)
+
+
+  function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
+
+
---
+++ src/app/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Sidebar from '../components/Layout/Sidebar'
+function Layout() {
+  return (
+    <div className='mainapp'>
+        <div className='sidepane'>
+            <Sidebar />
+        </div>
+        <div className='pagecontent'>
+            <Outlet />
+        </div>
+    </div>
+  )
+}
+
+export default Layout
